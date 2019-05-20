b6a5f3decb3d25f4d909454f62351b892d38f43b
poignant-minifier-0983 <davidfano@gmail.com>
2019-05-20 14:58:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -135,6 +135,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 "Assembly" << programmer_hash[:dennis_ritchie][:languages]
+		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 		 programmer_hash
 end
